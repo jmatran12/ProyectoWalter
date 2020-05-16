89f342e9455f3f3f2d62bb56bc5824101bf2391d
jmatran12 <javiermatranbartolome@hotmail.com>
2020-05-16 18:35:55 +0200

    Inicio TOTAL

--- src/proyecto/walter/PROYECTOWALTER.java
+++ src/proyecto/walter/PROYECTOWALTER.java
@@ -16,6 +16,7 @@ public class PROYECTOWALTER {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Sale en pantalla\n");
     }
     
 }
